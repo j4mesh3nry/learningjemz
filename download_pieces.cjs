a092d237aa21fd60c190841860f0c093094ba555
James Henry Emorricha <emorricha.jameshenry@gmail.com>
2026-08-01 17:01:17 +0800

    Fix squashed board and update piece SVG assets to standard cburnett style

--- download_pieces.cjs
+++ download_pieces.cjs
@@ -2,24 +2,26 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-const pieces = {
-    "w_k.svg": "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg",
-    "w_q.svg": "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg",
-    "w_r.svg": "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg",
-    "w_b.svg": "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_blt45.svg",
-    "w_n.svg": "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
-    "w_p.svg": "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
-    "b_k.svg": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg",
-    "b_q.svg": "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg",
-    "b_r.svg": "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg",
-    "b_b.svg": "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg",
-    "b_n.svg": "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg",
-    "b_p.svg": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg"
+const baseUrl = "https://raw.githubusercontent.com/lichess-org/lila/master/public/piece/cburnett/";
+
+const pieceMap = {
+    "w_k.svg": "wK.svg",
+    "w_q.svg": "wQ.svg",
+    "w_r.svg": "wR.svg",
+    "w_b.svg": "wB.svg",
+    "w_n.svg": "wN.svg",
+    "w_p.svg": "wP.svg",
+    "b_k.svg": "bK.svg",
+    "b_q.svg": "bQ.svg",
+    "b_r.svg": "bR.svg",
+    "b_b.svg": "bB.svg",
+    "b_n.svg": "bN.svg",
+    "b_p.svg": "bP.svg"
 };
 
 const options = {
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'NodeJS/14.0'
     }
 };
 
@@ -34,6 +36,14 @@ const download = (filename, url) => {
                     file.close();
                     resolve();
                 });
+            } else if (response.statusCode === 301 || response.statusCode === 302) {
+                https.get(response.headers.location, options, (res) => {
+                    res.pipe(file);
+                    file.on('finish', () => {
+                        file.close();
+                        resolve();
+                    });
+                });
             } else {
                 reject(new Error(`Failed to download ${url}: ${response.statusCode}`));
             }
@@ -44,11 +54,11 @@ const download = (filename, url) => {
 };
 
 async function downloadAll() {
-    console.log("Downloading chess pieces...");
-    for (const [filename, url] of Object.entries(pieces)) {
+    console.log("Downloading chess pieces from Lichess repo...");
+    for (const [localName, remoteName] of Object.entries(pieceMap)) {
         try {
-            await download(filename, url);
-            console.log(`Downloaded ${filename}`);
+            await download(localName, baseUrl + remoteName);
+            console.log(`Downloaded ${localName}`);
         } catch (e) {
             console.error(e.message);
         }
