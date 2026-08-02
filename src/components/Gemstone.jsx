73847191c3c75ccae4ab5cdc2b96fcc0e3067f58
James Henry Emorricha <emorricha.jameshenry@gmail.com>
2026-08-02 23:42:46 +0800

    fix: use solid colors for Gemstone to prevent svg gradient rendering issues

--- src/components/Gemstone.jsx
+++ src/components/Gemstone.jsx
@@ -7,48 +7,15 @@ export default function Gemstone({ className }) {
       className={className} 
       xmlns="http://www.w3.org/2000/svg"
     >
-      <defs>
-        {/* Gradients for a 3D faceted look */}
-        <linearGradient id="gem-top-center" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" stopColor="#86efac" />
-          <stop offset="100%" stopColor="#4ade80" />
-        </linearGradient>
-        
-        <linearGradient id="gem-top-left" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor="#4ade80" />
-          <stop offset="100%" stopColor="#22c55e" />
-        </linearGradient>
-        
-        <linearGradient id="gem-top-right" x1="100%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" stopColor="#4ade80" />
-          <stop offset="100%" stopColor="#16a34a" />
-        </linearGradient>
-        
-        <linearGradient id="gem-bottom-center" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" stopColor="#22c55e" />
-          <stop offset="100%" stopColor="#15803d" />
-        </linearGradient>
-
-        <linearGradient id="gem-bottom-left" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor="#16a34a" />
-          <stop offset="100%" stopColor="#166534" />
-        </linearGradient>
-        
-        <linearGradient id="gem-bottom-right" x1="100%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" stopColor="#15803d" />
-          <stop offset="100%" stopColor="#14532d" />
-        </linearGradient>
-      </defs>
-
       {/* Top Facets */}
-      <polygon points="30,20 70,20 60,40 40,40" fill="url(#gem-top-center)" />
-      <polygon points="10,40 30,20 40,40" fill="url(#gem-top-left)" />
-      <polygon points="90,40 70,20 60,40" fill="url(#gem-top-right)" />
+      <polygon points="30,20 70,20 60,40 40,40" fill="#6ee7b7" />
+      <polygon points="10,40 30,20 40,40" fill="#34d399" />
+      <polygon points="90,40 70,20 60,40" fill="#10b981" />
 
       {/* Bottom Facets */}
-      <polygon points="40,40 60,40 50,90" fill="url(#gem-bottom-center)" />
-      <polygon points="10,40 40,40 50,90" fill="url(#gem-bottom-left)" />
-      <polygon points="90,40 60,40 50,90" fill="url(#gem-bottom-right)" />
+      <polygon points="40,40 60,40 50,90" fill="#059669" />
+      <polygon points="10,40 40,40 50,90" fill="#047857" />
+      <polygon points="90,40 60,40 50,90" fill="#064e3b" />
       
       {/* Specular Highlights for extra shine */}
       <polygon points="30,20 70,20 66,24 34,24" fill="rgba(255,255,255,0.6)" />
